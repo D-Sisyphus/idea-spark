992342fea259f1c05551598d51dd0155b1ebe844
Divyansh Rathore <divyanshrathore175@gmail.com>
2026-02-02 00:46:31 +0530

    Fix routing for GitHub Pages using HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -19,18 +19,17 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin/*" element={<AdminDashboard />} />
-          <Route path="/teacher" element={<TeacherDashboard />} />
-          <Route path="/student" element={<StudentDashboard />} />
-          <Route path="/exam/:examId" element={<TakeExam />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+  <Route path="/" element={<Index />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+  <Route path="/admin/*" element={<AdminDashboard />} />
+  <Route path="/teacher" element={<TeacherDashboard />} />
+  <Route path="/student" element={<StudentDashboard />} />
+  <Route path="/exam/:examId" element={<TakeExam />} />
+  <Route path="*" element={<NotFound />} />
+</Routes>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
